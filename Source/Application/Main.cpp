5c066ff5f3bb2e873d35c4e2a4f00f663540e8f2
Olegh <khlystun.v@mail.ru>
2025-03-25 21:32:48 +0300

    readability has been raised

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -9,25 +9,35 @@ int main()
 {
   LineSegment segment({4, 5}, {2,5});
 
+  std::cout << "Line Segment start: " << std::endl;
   std::cout << "start = x: " << segment.start.x << std::endl;
   std::cout << std::setw(11) << "y: " << segment.start.y << std::endl;
 
   std::cout << "end = x: " << segment.end.x << std::endl;
   std::cout << std::setw(9) << "y: " << segment.end.y << std::endl;
+  std::cout << "Line Segment end.\n\n";
+
+  std::cout << "Broken Line start :" << std::endl;
 
   BrokenLine brokenLine({segment});
 
-  std::cout << "broken line length: " << brokenLine.Length() << std::endl;
+  std::cout << "length: " << brokenLine.Length() << std::endl;
   
-  for (const LineSegment & segment : brokenLine.GetSegments())
+  const std::vector<LineSegment> lineSegments = brokenLine.GetSegments();
+  for (const LineSegment & segment : lineSegments)
   {
-    std::cout << "broken line segment start point x y: " << segment.start.x << " " << segment.end.y << std::endl;
-    std::cout << "broken line segment end point x y: " << segment.end.x << " " << segment.end.y << std::endl << std::endl;
+    std::cout << "segment start x y: " << segment.start.x << " " << segment.start.y << std::endl;
+    std::cout << "segment end x y: " << segment.end.x << " " << segment.end.y << std::endl;
   }
 
+  std::cout << "Broken Line end.\n\n";
+
+  std::cout << "Rectangle start: " << std::endl;
   Rectangle rect({ 1, 2 }, {3, 4});
 
   std::cout << "rect top left x y: " << rect.TopLeft().x << " " << rect.TopLeft().y << std::endl;
   std::cout << "rect bottom right x y: " << rect.BottomRight().x << " " << rect.BottomRight().y << std::endl;
+
+  std::cout << "Rectangle end.\n\n";
 
 }
